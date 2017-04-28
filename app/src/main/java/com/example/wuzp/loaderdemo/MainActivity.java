2323fb3becfee3338464a9cd9a077149fd67bcb2
wzp09tjlg <wzp09tjlg@163.com>
2017-04-28 15:54:29 +0800

    1.init project

---
+++ app/src/main/java/com/example/wuzp/loaderdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wuzp.loaderdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
